798287b9540c9ec463c5370635048494c102e8ec
Sateek Roy <sateeksuper@gmail.com>
2020-08-07 00:40:37 -0400

    Updates

---
+++ precache-manifest.fcc75b94e80c4e06c19a42e67993b348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9a559938d3adc0f78493e5439b3e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c917375d9f70a623ba",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f9ee3c9ecf9268fb1aa3",
+    "url": "/static/js/2.abf8c55d.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.abf8c55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c917375d9f70a623ba",
+    "url": "/static/js/main.4b082e38.chunk.js"
+  },
+  {
+    "revision": "f302f149776224936f2c",
+    "url": "/static/js/runtime-main.2a41bda1.js"
+  }
+]);
